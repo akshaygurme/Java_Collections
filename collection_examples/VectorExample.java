94ee9c5f067857b5710ac7ca4b2e80322123a06b
akshaygurme <42416798+akshaygurme@users.noreply.github.com>
2024-01-08 23:40:35 +0530

    vector example added

---
+++ collection_examples/VectorExample.java
@@ -0,0 +1,16 @@
+package collection_examples;
+
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+
+        vector.add(10);
+        vector.add(11);
+        vector.add(9);
+
+        System.out.println(vector);
+
+    }
+}
